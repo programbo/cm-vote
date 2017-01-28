a52d776b01657fc6900392b206218b0cd893a724
John Lombardo <john@programbo.com>
2017-01-28 16:46:28 +0800

    Add a graph

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -1,8 +1,17 @@
 'use strict';
 
-var parseChartData = function parseChartData(_ref) {
-  var results = _ref.results;
+var getCampaignId = function getCampaignId() {
+  var parsedSearch = location.search.match(/cid=([0-9a-f]{32})/i);
+  if (parsedSearch.length === 2) {
+    return parsedSearch[1];
+  }
+};
 
+var updateCampaginInfo = function updateCampaginInfo(campaign) {
+  console.log('updateCampaginInfo', campaign); // eslint-disable-line no-console
+};
+
+var parseChartData = function parseChartData(results) {
   var labels = Object.keys(results);
   var data = labels.map(function (label) {
     return results[label];
@@ -10,7 +19,16 @@ var parseChartData = function parseChartData(_ref) {
   return {
     labels: labels,
     datasets: [{
-      data: data
+      data: data,
+      backgroundColor: labels.map(function (label, index) {
+        return 'hsl(300, 85%, ' + (100 - 15 * index) + '%)';
+      }),
+      borderWidth: labels.map(function () {
+        return 4;
+      }),
+      borderColor: labels.map(function () {
+        return 'hsl(300, 85%, 25%)';
+      })
     }]
   };
 };
@@ -23,9 +41,17 @@ var redirectWithCampaignId = function redirectWithCampaignId(cid, location) {
   location.href = protocol + '//' + host + pathname + '?cid=' + cid;
 };
 
-var activateCampaignsMenu = function activateCampaignsMenu() {
+var activateCampaignsMenu = function activateCampaignsMenu(campaigns) {
   var campaignsMenu = document.getElementById('campaigns');
   if (campaignsMenu) {
+    campaigns.forEach(function (_ref) {
+      var CampaignID = _ref.CampaignID,
+          Name = _ref.Name,
+          SentDate = _ref.SentDate;
+
+      $('<option value="' + CampaignID + '">' + Name + ' (Sent ' + SentDate + ')</option>').appendTo(campaignsMenu);
+    });
+    campaignsMenu.style.display = 'block';
     campaignsMenu.addEventListener('change', function () {
       var campaignId = campaignsMenu.querySelector(':checked').value;
       redirectWithCampaignId(campaignId, location);
@@ -33,32 +59,73 @@ var activateCampaignsMenu = function activateCampaignsMenu() {
   }
 };
 
-var fetchChartData = function fetchChartData(cid, success) {
+var fetchCampaignData = function fetchCampaignData() {
+  var cid = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+  var success = arguments[1];
+  var error = arguments[2];
+
   $.ajax({
     type: 'GET',
-    url: 'http://john.local:3000/api',
+    url: '/api',
     data: { cid: cid },
     success: success,
-    error: function error(xhr, type) {
+    error: error || function (xhr, type) {
       console.log('xhr', xhr); // eslint-disable-line no-console
       console.log('type', type); // eslint-disable-line no-console
     }
   });
 };
 
-var drawResults = function drawResults() {
-  var chart = document.getElementById('chart');
-  if (chart) {
-    var cid = chart.getAttribute('data-cid');
-    if (cid) {
-      fetchChartData(cid, function (results) {
-        console.log('results', results); // eslint-disable-line no-console
-      });
+var drawResults = function drawResults(type, results, _ref2) {
+  var Name = _ref2.Name;
+
+  var data = parseChartData(results);
+  var myPieChart = new Chart('chart', {
+    type: type,
+    data: data,
+    options: {
+      title: {
+        display: true,
+        text: Name,
+        fontFamily: 'Averta W01 Black',
+        fontSize: 36,
+        padding: 30,
+        fontColor: '#fff'
+      },
+      legend: {
+        position: 'bottom',
+        labels: {
+          fontSize: 16,
+          fontFamily: 'Averta W01 Black',
+          padding: 30,
+          fontColor: '#fff'
+        }
+      },
+      tooltips: {
+        titleFontSize: 18,
+        bodyFontSize: 18,
+        xPadding: 10,
+        yPadding: 10,
+        caretSize: 9
+      },
+      animation: {
+        duration: 300
+      }
     }
-  }
+  });
 };
 
 $(function () {
-  activateCampaignsMenu();
-  drawResults();
+  var campaignId = getCampaignId();
+  fetchCampaignData(campaignId, function (_ref3) {
+    var campaign = _ref3.campaign,
+        campaigns = _ref3.campaigns,
+        results = _ref3.results;
+
+    if (campaigns) {
+      activateCampaignsMenu(campaigns);
+    } else if (results && campaign) {
+      drawResults('pie', results, campaign);
+    }
+  });
 });
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,10 +1,24 @@
-const parseChartData = ({ results }) => {
+const getCampaignId = () => {
+  const parsedSearch = location.search.match(/cid=([0-9a-f]{32})/i);
+  if (parsedSearch.length === 2) {
+    return parsedSearch[1];
+  }
+};
+
+const updateCampaginInfo = (campaign) => {
+  console.log('updateCampaginInfo', campaign); // eslint-disable-line no-console
+};
+
+const parseChartData = (results) => {
   const labels = Object.keys(results);
   const data = labels.map((label) => results[label]);
   return {
     labels,
     datasets: [{
-      data
+      data,
+      backgroundColor: labels.map((label, index) => `hsl(300, 85%, ${100-(15*index)}%)`),
+      borderWidth: labels.map(() => 4),
+      borderColor: labels.map(() => 'hsl(300, 85%, 25%)')
     }]
   }
 };
@@ -14,42 +28,78 @@ const redirectWithCampaignId = (cid, location) => {
   location.href = `${protocol}//${host}${pathname}?cid=${cid}`;
 };
 
-const activateCampaignsMenu = () => {
+const activateCampaignsMenu = (campaigns) => {
   const campaignsMenu = document.getElementById('campaigns');
   if (campaignsMenu) {
+    campaigns.forEach(({ CampaignID, Name, SentDate }) => {
+      $(`<option value="${CampaignID}">${Name} (Sent ${SentDate})</option>`).appendTo(campaignsMenu);
+    });
+    campaignsMenu.style.display = 'block';
     campaignsMenu.addEventListener('change', () => {
       const campaignId = campaignsMenu.querySelector(':checked').value;
       redirectWithCampaignId(campaignId, location);
     });
   }
-}
+};
 
-const fetchChartData = (cid, success) => {
+const fetchCampaignData = (cid = '', success, error) => {
   $.ajax({
     type: 'GET',
-    url: 'http://john.local:3000/api',
+    url: '/api',
     data: { cid },
     success,
-    error: (xhr, type) => {
+    error: error || ((xhr, type) => {
       console.log('xhr', xhr); // eslint-disable-line no-console
       console.log('type', type); // eslint-disable-line no-console
-    }
+    })
   });
 }
 
-const drawResults = () => {
-  const chart = document.getElementById('chart');
-  if (chart) {
-    const cid = chart.getAttribute('data-cid');
-    if (cid) {
-      fetchChartData(cid, (results) => {
-        console.log('results', results); // eslint-disable-line no-console
-      });
+const drawResults = (type, results, { Name }) => {
+  const data = parseChartData(results);
+  const myPieChart = new Chart('chart', {
+    type,
+    data,
+    options: {
+      title: {
+        display: true,
+        text: Name,
+        fontFamily: 'Averta W01 Black',
+        fontSize: 36,
+        padding: 30,
+        fontColor: '#fff'
+      },
+      legend: {
+        position: 'bottom',
+        labels: {
+          fontSize: 16,
+          fontFamily: 'Averta W01 Black',
+          padding: 30,
+          fontColor: '#fff'
+        }
+      },
+      tooltips: {
+        titleFontSize: 18,
+        bodyFontSize: 18,
+        xPadding: 10,
+        yPadding: 10,
+        caretSize: 9
+      },
+      animation: {
+        duration: 300
+      }
     }
-  }
-}
+  });
+};
 
 $(() => {
-  activateCampaignsMenu();
-  drawResults();
+  const campaignId = getCampaignId();
+  fetchCampaignData(campaignId, ({ campaign, campaigns, results }) => {
+    if (campaigns) {
+      activateCampaignsMenu(campaigns);
+    }
+    else if (results && campaign) {
+      drawResults('pie', results, campaign);
+    }
+  });
 });
